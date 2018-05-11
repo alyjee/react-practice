5990126bff37d0c1735c21ef08d7393e6c065d23
Tahir <tahir.raza@ilsainteractive.com>
2018-05-11 12:02:04 +0500

    first commit for react practice

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Game from './Game';
+
+class App extends Component {
+  render() {
+    return (
+      <Game />
+    );
+  }
+}
+
+export default App;
